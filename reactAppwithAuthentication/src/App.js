621d4599560fe1702fbe04fe6d35bf8ea8990ff0
Jaskeerat Singh <jaskeerat10000@gmail.com>
2021-08-06 20:51:08 +0530

    set up the basic auth0 structure in app

---
+++ reactAppwithAuthentication/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import LoginButton from './components/LoginButton';
+
+function App() {
+  return (
+    <div className="App">
+          <LoginButton />
+    </div>
+  );
+}
+
+export default App;
